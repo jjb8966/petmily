baa7e99f76fa2c74f741faf2d0298b47131ecae1
jjb8966 <jjb8966@naver.com>
2022-11-18 22:45:38 +0900

    refactor: 마이페이지에서 회원 등급을 한글로 출력하도록 수정 - MemberGrade 클래스에 description 필드 추가

--- src/main/java/com/petmily/domain/core/enum_type/MemberGrade.java
+++ src/main/java/com/petmily/domain/core/enum_type/MemberGrade.java
@@ -1,5 +1,14 @@
 package com.petmily.domain.core.enum_type;
 
+import lombok.Getter;
+
+@Getter
 public enum MemberGrade {
-    NORMAL, ADMIN
+    NORMAL("일반 회원"), ADMIN("관리자");
+
+    private String description;
+
+    MemberGrade(String description) {
+        this.description = description;
+    }
 }

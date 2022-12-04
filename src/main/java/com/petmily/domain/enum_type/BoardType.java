ae46b37bd8b99b0da62b3fab6721c004b9c556a4
jjb8966 <jjb8966@naver.com>
2022-12-04 23:10:04 +0900

    chore: BoardType description 수정

--- src/main/java/com/petmily/domain/enum_type/BoardType.java
+++ src/main/java/com/petmily/domain/enum_type/BoardType.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public enum BoardType {
     FREE("자유 게시판"), INQUIRY("문의 게시판"), ADOPT_REVIEW("입양 후기 게시판"),
-    FIND("찾아요 게시판"), WATCH("봤어요 게시판");
+    FIND("반려동물 찾아요 게시판"), WATCH("유기동물 봤어요 게시판");
 
     private String description;
 

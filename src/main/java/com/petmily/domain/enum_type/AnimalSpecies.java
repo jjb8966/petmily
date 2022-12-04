bea5b1c57ea9bd5c84850876e10c3fd959d884f1
jjb8966 <jjb8966@naver.com>
2022-12-04 23:09:18 +0900

    refactor: AnimalSpecies에 description 필드 추가

--- src/main/java/com/petmily/domain/enum_type/AnimalSpecies.java
+++ src/main/java/com/petmily/domain/enum_type/AnimalSpecies.java
@@ -1,5 +1,15 @@
 package com.petmily.domain.enum_type;
 
+import lombok.Getter;
+
+@Getter
 public enum AnimalSpecies {
-    DOG, CAT, ETC
+    DOG("강아지"), CAT("고양이"), ETC("기타");
+
+    private String description;
+
+    AnimalSpecies(String description) {
+        this.description = description;
+    }
+
 }

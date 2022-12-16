0bf09d784c686a851a2cbef1fe2b674c0989fbd0
jjb8966 <jjb8966@naver.com>
2022-12-17 00:20:36 +0900

    feat: 찾아요/봤어요 게시판 매칭 기능 구현

--- src/main/java/com/petmily/domain/core/board/FindWatchBoard.java
+++ src/main/java/com/petmily/domain/core/board/FindWatchBoard.java
@@ -7,13 +7,20 @@ import com.petmily.domain.enum_type.FindWatchBoardStatus;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Slf4j
 public class FindWatchBoard extends Board {
 
     private LocalDateTime lostOrWatchTime;
@@ -23,6 +30,12 @@ public class FindWatchBoard extends Board {
     private String animalKind;
     private Integer animalAge;
     private Float animalWeight;
+
+    @OneToMany(mappedBy = "targetBoard", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<MatchInfo> matchedBoardInfos = new ArrayList<>();
+
+    @OneToMany(mappedBy = "matchedBoard", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<MatchInfo> targetBoardInfos = new ArrayList<>();
 
     public FindWatchBoard(FindWatchBoardBuilder builder) {
         super(builder);
@@ -46,6 +59,38 @@ public class FindWatchBoard extends Board {
         this.animalWeight = form.getAnimalWeight();
     }
 
+    public void addTargetBoardInfo(MatchInfo matchInfo) {
+        targetBoardInfos.add(matchInfo);
+    }
+
+    public void addMatchedBoardInfo(MatchInfo matchInfo) {
+        matchedBoardInfos.add(matchInfo);
+    }
+
+    public List<FindWatchBoard> getMatchBoards() {
+        List<FindWatchBoard> result = new ArrayList<>();
+
+        List<FindWatchBoard> matchedBoardList = getMatchedBoards();
+        List<FindWatchBoard> targetBoardList = getTargetBoards();
+
+        result.addAll(matchedBoardList);
+        result.addAll(targetBoardList);
+
+        return result;
+    }
+
+    private List<FindWatchBoard> getTargetBoards() {
+        return targetBoardInfos.stream()
+                .map(MatchInfo::getTargetBoard)
+                .collect(Collectors.toList());
+    }
+
+    private List<FindWatchBoard> getMatchedBoards() {
+        return matchedBoardInfos.stream()
+                .map(MatchInfo::getMatchedBoard)
+                .collect(Collectors.toList());
+    }
+
     @Override
     public String toString() {
         return "FindWatchBoard{" +
@@ -56,6 +101,16 @@ public class FindWatchBoard extends Board {
                 ", animalKind='" + animalKind + '\'' +
                 ", animalAge=" + animalAge +
                 ", animalWeight=" + animalWeight +
+                ", matchedBoardInfos=" + matchedBoardInfos.size() +
+                ", targetBoardInfos=" + targetBoardInfos.size() +
+                ", id=" + id +
+                ", member=" + member.getName() +
+                ", replies=" + replies.size() +
+                ", pictures=" + pictures.size() +
+                ", boardType=" + boardType +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", shownAll=" + shownAll +
                 '}';
     }
 }
